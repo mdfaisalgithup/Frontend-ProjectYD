bc8ca675343f2dc5dfcf05eb876da8578dba9ee2
mdfaisalgithupe <mdfaisald0706@gmail.com>
2025-09-13 23:36:12 +0600

    updateds

--- src/app/Component/Home-Section/homeSection.jsx
+++ src/app/Component/Home-Section/homeSection.jsx
@@ -134,7 +134,7 @@ setTotalProgress(0)
 
 
 <div className='mt-6'>
-   <h2 className='font-semibold text-center'>Download List -</h2>
+   <h2 className='font-semibold text-center my-2'>Download List -</h2>
         <div className='w-full flex justify-center items-center border-2 border-dotted border-blue-200 rounded-md py-2 px-4 shadow-md'>
        
          
@@ -144,6 +144,7 @@ setTotalProgress(0)
               downloadData?.length !== 0 ?
                 <div className='py-2'>
                   {downloadData?.title && <h2 className='font-semibold py-3 pr-10'> Title: {downloadData?.title}...</h2>}
+                 
                   <Image
                     src={`${downloadData?.vthumbnail}`}
                     width={400}
@@ -151,6 +152,7 @@ setTotalProgress(0)
                     alt="Video Thumbnail"
                   />
 
+                 
                   <ul>
                     {downloadData?.formats?.map((format) => (
                       <li className='border-1 my-1 hover:bg-blue-100 border-[#0e0e0e2f] p-3' key={format.itag}>
@@ -212,22 +214,22 @@ setTotalProgress(0)
 
       {/* section... start*/}
      <div>
-      <h2 className='text-center my-2 font-semibold'>All Format Download</h2>
+      <h2 className='text-center mt-10 mb-4 font-semibold'>All Format Download</h2>
        <div className=' grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 grid-cols-1 space-x-2'>
 
-       <div className='hover:bg-amber-200 p-8 duration-500 bg-amber-100 hover:shadow-2xl'>
+       <div className='hover:bg-[#ededed83] rounded-md p-8 duration-500 border-1 border-[#ecebeb] hover:shadow shadow-md'>
          <h2 className='font-semibold my-2 text-center'>Best Audio HD Quality</h2>
-        <p>Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, </p>
+        <p>Download YouTube videos in the best HD audio quality with our free YouTube Downloader. Convert your favorite songs, podcasts, and videos to high-quality MP3, WAV, or AAC formats instantly. Our fast, secure, and easy-to-use platform works on all devices, including Windows, Mac, Android, and iOS. Simply paste the YouTube link, choose the audio format, and get crystal-clear sound without losing quality. Enjoy offline listening anytime, anywhere. No registration, no limits, completely free, and optimized for smooth, high-quality audio downloads from YouTube.</p>
        </div>
 
-       <div className='hover:bg-amber-200 p-8 duration-500 bg-amber-100  hover:hover:shadow-2xl hover:shadow-'>
+       <div className='hover:bg-[#ededed83] p-8 rounded-md duration-500 border-1 border-[#ecebeb] hover:shadow shadow-md'>
          <h2 className='font-semibold my-2 text-center'>1080p Full HD</h2>
-        <p>Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality,Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality,Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, </p>
+        <p>Download YouTube videos in stunning 1080p Full HD with our fast and free YouTube downloader. Save your favorite videos in high-quality MP4, MP3, or other formats with just one click. Our platform is user-friendly, works on all devices, and requires no registration. Enjoy offline access to music, movies, tutorials, and more without compromising video quality. With lightning-fast downloads and full compatibility across Windows, Mac, Android, and iOS, our 1080p YouTube downloader makes it easy to keep your favorite content anytime, anywhere. Safe, secure, and completely free.</p>
        </div>
 
-       <div className='hover:bg-amber-200 p-8 duration-500 bg-amber-100 hover:shadow-2xl'>
+       <div className='hover:bg-[#ededed83] p-8 rounded-md duration-500 border-1 border-[#ecebeb] hover:shadow shadow-md'>
          <h2 className='font-semibold my-2 text-center'>4k HD Download</h2>
-        <p>Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality,Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality,Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, </p>
+        <p>Download YouTube videos in stunning 4K HD with our free YouTube Downloader. Convert and save videos in high-quality MP4, MP3, or other popular formats instantly. Our fast, safe, and user-friendly platform works on all devices, including Windows, Mac, Android, and iOS. No registration or software installation is required—just paste the video URL, select 4K HD quality, and download. Perfect for offline viewing of music, movies, tutorials, and more. Experience the ultimate 4K HD YouTube downloader for free, anytime, anywhere.</p>
        </div>
 
 
@@ -237,22 +239,22 @@ setTotalProgress(0)
 
            {/* section... start*/}
      <div>
-      <h2 className='text-center my-8 font-semibold'>All Device Supported</h2>
-       <div className='  grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 grid-cols-1 space-x-4'>
+      <h2 className='text-center mt-15 mb-4 font-semibold'>All Device Supported</h2>
+       <div className='  grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 grid-cols-1 space-x-4 '>
 
-       <div className='hover:bg-amber-200 p-8 duration-500 shadow-md hover:shadow-[0px] border-[1px] border-[#eee]'>
-         <h2 className='font-semibold my-2 text-center'>Small Phone</h2>
-        <p>Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, </p>
+       <div className='hover:bg-[#fff9d8fe] rounded-md p-8 duration-500 shadow-md hover:shadow-[0px] border-[1px] border-[#eee]'>
+         <h2 className='font-semibold my-2 text-center uppercase'>Small Phone Support</h2>
+        <p>Download YouTube videos easily on your small phone with our All Format YouTube Downloader. Quickly save videos in MP4, MP3, AVI, and more, directly to your mobile device without any software. Our lightweight, mobile-friendly platform works on Android and iOS, letting you watch music, tutorials, movies, and favorite content offline anytime. No registration, no limits, and completely free to use. Enjoy fast, safe, and hassle-free downloads on your small phone. Perfect for users who want YouTube videos on the go, anytime, anywhere.</p>
        </div>
 
-       <div className='bg-amber-200 p-8 duration-500 shadow-md hover:shadow-[0px] border-[1px] border-[#eee]'>
-         <h2 className='font-semibold my-2 text-center'>Smart-Phone</h2>
-        <p>Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality,Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality,Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, </p>
+       <div className='hover:bg-[#fff9d8fe] rounded-md p-8 duration-500 shadow-md hover:shadow-[0px] border-[1px] border-[#eee]'>
+         <h2 className='font-semibold my-2 text-center uppercase'>Andoid/Iphone All Support</h2>
+        <p>Download YouTube videos directly to your smartphone with our All Format YouTube Downloader. Supporting MP4, MP3, AVI, and more, our platform makes it easy to save videos for offline viewing on Android and iOS devices. Simply paste the YouTube link, choose your preferred format and quality, and start downloading instantly. No app installation or registration required. Enjoy fast, safe, and high-quality downloads of music, tutorials, movies, and more, all optimized for mobile devices. Access your favorite content anytime, anywhere with ease.</p>
        </div>
 
-       <div className='hover:bg-amber-200 p-8 duration-500 shadow-md hover:shadow-[0px] border-[1px] border-[#eee]'>
-         <h2 className='font-semibold my-2 text-center'>Laptop/PC</h2>
-        <p>Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality,Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality,Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, Best Audio Quality, </p>
+       <div className='hover:bg-[#fff9d8fe] rounded-md p-8 duration-500 shadow-md hover:shadow-[0px] border-[1px] border-[#eee]'>
+         <h2 className='font-semibold my-2 text-center uppercase'>Laptop/PC Support</h2>
+        <p>Download YouTube videos on your Laptop or PC effortlessly with our All Format YouTube Downloader. Convert YouTube videos to MP4, MP3, AVI, and other popular formats quickly and safely. Our website is fully compatible with Windows, Mac, and Linux, allowing you to save high-quality videos, music, tutorials, and movies directly to your computer. No software installation or registration is required. Enjoy unlimited downloads, offline access, and fast performance. The best YouTube downloader for Laptop and PC users seeking free, secure, and reliable video conversion.</p>
        </div>
 
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-   
+   <link rel="icon" href="https://i.ibb.co.com/5WS5gF5d/play-button-7426443-960-720.webp" sizes="any" />
       <body>
         {children}
       </body>
