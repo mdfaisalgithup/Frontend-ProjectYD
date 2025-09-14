fc94cffcff3e5c9ff43f4fcf4b89b4856862facb
mdfaisalgithupe <mdfaisald0706@gmail.com>
2025-09-14 08:47:30 +0600

    g

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-   <link rel="icon" href="https://i.ibb.co.com/5WS5gF5d/play-button-7426443-960-720.webp" sizes="any" />
+      <head> 
+        <link rel="icon" href="https://i.ibb.co.com/5WS5gF5d/play-button-7426443-960-720.webp" sizes="any" />
+      <meta name="google-site-verification" content="O6j5me_hO-sbgizHeDrAUNy52rvJ72nRkJew0w_VFyc" />
+      </head>
+  
       <body>
         {children}
       </body>
