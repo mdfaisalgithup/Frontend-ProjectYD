622cc47a2cc6e1f928d7a5c6fb93ee9a0f436510
mdfaisalgithupe <mdfaisald0706@gmail.com>
2025-09-14 08:49:40 +0600

    H

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import './globals.css';
 
 
@@ -18,10 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head> 
+     <Head>
         <link rel="icon" href="https://i.ibb.co.com/5WS5gF5d/play-button-7426443-960-720.webp" sizes="any" />
       <meta name="google-site-verification" content="O6j5me_hO-sbgizHeDrAUNy52rvJ72nRkJew0w_VFyc" />
-      </head>
+   
+     </Head>
   
       <body>
         {children}
